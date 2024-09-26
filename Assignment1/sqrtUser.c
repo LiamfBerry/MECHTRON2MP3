a7c90a249be6f8bf034eae4a5bb7060665a80e30
Liam <Liamberry06@gmail.com>
2024-09-25 20:41:59 -0400

    0v0.0

---
+++ Assignment1/sqrtUser.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+double sqrtUser(double number, int n) {
+
+    double root = number/2;
+    double precision = 0.1;
+
+    for (int i=0; i<n; i++) {
+        precision /= 10;
+    }
+
+    while (true) {
+        
+        if (root*root-number < precision || number==0) {
+            break;
+        }
+        else if (number<0) {
+            printf("enter a valid number\n");
+            return 0;
+        }
+        root = root-(root*root-number)/(2*root);
+    }  
+    return root;
+}
+
+
+int main() {
+    double number = 0;
+    int decimals = 1;
+    printf("Take the square root of: ");
+    scanf("%lf", &number);
+    printf("Accurate to how many decimals: ");
+    scanf("%d", &decimals);
+    printf("The square root is %0.*f\n", decimals, sqrtUser(number,decimals));
+}
