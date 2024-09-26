48566101e6175de0640bf76512e71cdf507b877f
Liam <Liamberry06@gmail.com>
2024-09-26 01:40:25 -0400

    1v1.1

--- Assignment1/timeComparison.c
+++ Assignment1/timeComparison.c
@@ -4,10 +4,15 @@
 
 int main() {
 
+    double number = 0;
+
+    printf("Number to take square root of: ");
+    scanf("%lf", &number);
+
     clock_t start, end;
 
     start = clock();
-    double x = sqrt(0.5);
+    double x = sqrt(number);
     end = clock();
     printf("Square root is: %lf\n", x);
     printf("Time to run: %lf\n", (double)(end-start)/ CLOCKS_PER_SEC);
